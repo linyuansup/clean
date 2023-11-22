85cf24e37446ec8b64990a1826b9539f2f9894fa
linyuansup <linyuansup@gmail.com>
2023-11-22 13:04:05 +0800

    重写errorcode

--- src/main/kotlin/ArgsException.kt
+++ src/main/kotlin/ArgsException.kt
@@ -12,42 +12,5 @@ class ArgsException(var errorCode: ErrorCode, errorArgumentId: Char? = null, err
         }
     }
 
-    fun errorMessage() = when (errorCode) {
-        ErrorCode.OK -> "TILT: Should not get here."
-        ErrorCode.UNEXPECTED_ARGUMENT -> String.format("Argument -%c unexpected.", errorArgumentId)
-        ErrorCode.MISSING_STRING -> String.format(
-            "Could not find string parameter for -%c.",
-            errorArgumentId
-        )
-
-        ErrorCode.INVALID_INTEGER -> String.format(
-            "Argument -%c expects an integer but was %s.",
-            errorArgumentId, errorParameter
-        )
-
-        ErrorCode.MISSING_INTEGER -> String.format(
-            "Could not find integer parameter for -%c.",
-            errorArgumentId
-        )
-
-        ErrorCode.INVALID_DOUBLE -> String.format(
-            "Argument -%c expects an double but was %s.",
-            errorArgumentId, errorParameter
-        )
-
-        ErrorCode.MISSING_DOUBLE -> String.format(
-            "Could not find double parameter for -%c.",
-            errorArgumentId
-        )
-
-        ErrorCode.INVALID_ARGUMENT_FORMAT -> String.format(
-            "Invalid argument format for -%c.",
-            errorArgumentId
-        )
-
-        ErrorCode.INVALID_ARGUMENT_NAME -> String.format(
-            "Invalid argument name for -%c.",
-            errorArgumentId
-        )
-    }
+    fun errorMessage() = errorCode.message(errorArgumentId, errorParameter)
 }
--- src/main/kotlin/ErrorCode.kt
+++ src/main/kotlin/ErrorCode.kt
@@ -1,6 +1,58 @@
-enum class ErrorCode {
-    OK, INVALID_ARGUMENT_FORMAT, UNEXPECTED_ARGUMENT, INVALID_ARGUMENT_NAME,
-    MISSING_STRING,
-    MISSING_INTEGER, INVALID_INTEGER,
-    MISSING_DOUBLE, INVALID_DOUBLE
+enum class ErrorCode(val message: (errorArgumentId: Char, errorParameter: String) -> String) {
+    OK({ _, _ ->
+        "TILT: Should not get here."
+    }),
+
+    INVALID_ARGUMENT_FORMAT({ errorArgumentId, _ ->
+        String.format(
+            "Invalid argument format for -%c.",
+            errorArgumentId
+        )
+    }),
+
+    UNEXPECTED_ARGUMENT({ errorArgumentId, _ ->
+        String.format("Argument -%c unexpected.", errorArgumentId)
+    }),
+
+    INVALID_ARGUMENT_NAME({ errorArgumentId, _ ->
+        String.format(
+            "Invalid argument name for -%c.",
+            errorArgumentId
+        )
+    }),
+
+    MISSING_STRING({ errorArgumentId, _ ->
+        String.format(
+            "Could not find string parameter for -%c.",
+            errorArgumentId
+        )
+    }),
+
+    MISSING_INTEGER({ errorArgumentId, _ ->
+        String.format(
+            "Could not find integer parameter for -%c.",
+            errorArgumentId
+        )
+    }),
+
+    INVALID_INTEGER({ errorArgumentId, errorParameter ->
+        String.format(
+            "Argument -%c expects an integer but was %s.",
+            errorArgumentId, errorParameter
+        )
+    }),
+
+    MISSING_DOUBLE({ errorArgumentId, _ ->
+        String.format(
+            "Could not find double parameter for -%c.",
+            errorArgumentId
+        )
+    }),
+
+    INVALID_DOUBLE({ errorArgumentId, errorParameter ->
+        String.format(
+            "Argument -%c expects an double but was %s.",
+            errorArgumentId, errorParameter
+        )
+    }),
 }
